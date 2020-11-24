e44d06e37a4769e510300f6c19a99d084eb20dca
Ste Griffiths <stephen.griffiths@nsg.com>
2020-11-24 17:08:25 +0000

    Filter front page

--- src/app/army/unit-list/unit-list.component.ts
+++ src/app/army/unit-list/unit-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Unit } from 'app/army/unit';
 import { UnitService } from 'app/army/unit.service';
 
@@ -8,14 +9,22 @@ import { UnitService } from 'app/army/unit.service';
   styleUrls: ['./unit-list.component.css']
 })
 export class UnitListComponent implements OnInit {
+ 
+  filterForm = new FormGroup({
+    "egyptian" : new FormControl(true),
+    "greek" : new FormControl(true),
+    "norse" : new FormControl(true)
+  });
 
   constructor(public unitService: UnitService) {
   }
 
-  public allUnits: Unit[];
-
   ngOnInit(): void {
-    this.allUnits = this.unitService.getAllUnits();
   }
 
+  filteredUnits() : Unit[] {
+    console.log(this.filterForm.value);
+    return this.unitService.getAllUnits().filter(u => this.filterForm.value[u.race]);
+  }
+ 
 }
